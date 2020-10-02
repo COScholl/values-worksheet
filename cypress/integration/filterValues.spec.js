1938d04ee4c5949b22a61cc1fb2f81a3e0b077ee
Chris Scholl <chris.o.scholl@gmail.com>
2020-10-02 18:25:00 -0400

    value_card first commit with new test spec 	added gherkin-style comments from which to build tests

---
+++ cypress/integration/filterValues.spec.js
@@ -0,0 +1,15 @@
+/**
+ * Background
+ *  Given a list of values
+ *  And when presented with each value from the list
+ * Scenario - confirm the value resonates with me
+ *  When I am presented with a value that resonates with me
+ *  And I click 'Yes'
+ *  Then I should get confirmation that the value was accepted
+ *  And that value should remain in the list
+ * Scenario - deny the value resonates with me
+ *  When I am presented with a value that does not resonate with me
+ *  And I click 'No'
+ *  Then I should get confirmation that the value was not accepted
+ *  And that value should be removed from the list
+ */
